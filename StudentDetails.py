82285308cc3fe30a99c1f8fe2ece5b63889e9c84
rameshpython <ramababugaganam5515@gmail.com>
2025-05-25 11:59:19 +0530

    added last name

--- StudentDetails.py
+++ StudentDetails.py
@@ -3,10 +3,15 @@ studentClass=4
 stundetRollNumber=12
 surname="gaganam"
 firstName="ramesh"
+lastName ="babu"
+
+
 print(studentClass)
 print(stundetRollNumber)
 print(surname)
 print(firstName)
+print(lastName)
+
 
 
 
