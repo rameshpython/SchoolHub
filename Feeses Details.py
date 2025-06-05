cbadce5098de97037524970ac09d4bdecf12c464
rameshpython <rambabugaganam5515@gmail.com>
2025-06-05 10:31:10 +0530

    Add files via upload
    added fees details to schoolHub python

---
+++ Feeses Details.py
@@ -0,0 +1,45 @@
+student_name ="Ravi Kumar"
+roll_number ="A1034"
+course ="python programming"
+yearly_fees ="100000"
+student_fees_anum_once ="90000"
+exam_fees =5000.0
+fees_paid =3000.0
+lab_fees =8000
+transport_fees =50000
+admission_fees =25000
+damageditem_fees =10000
+uniform_fees =2500
+books_fees =3000
+bus_fees =33000
+extraactivity_fees =1000
+execursion_fees =2000
+irregular_fees =500
+idcard_fees =200
+dairy_fees=150
+
+
+
+
+
+print(student_name)
+print(roll_number)
+print(course)
+print(yearly_fees)
+print(student_fees_anum_once)
+print(exam_fees,type(exam_fees))
+print(fees_paid,type(fees_paid))
+print(lab_fees)
+print(transport_fees)
+print(admission_fees)
+print(damageditem_fees)
+print(uniform_fees)
+print(books_fees)
+print(bus_fees)
+print(extraactivity_fees)
+print(execursion_fees)
+print(irregular_fees)
+print(idcard_fees)
+print(dairy_fees)   
+
+
