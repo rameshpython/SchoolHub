b9a00d22fab4e42c68c7650528238f2a6749bf46
rameshpython <rambabugaganam5515@gmail.com>
2025-06-05 10:34:16 +0530

    Add files via upload
    added staff  management in schoolHub python

---
+++ Staff Management.py
@@ -0,0 +1,43 @@
+Staff_list =[{"Staff ID":1,"Name":"Ravi",
+              "Role":"Teacher","Salary":30000},
+             {"Staff ID":2,"Name":"Rakesh",
+              "Role":"Principal","Salary":50000},
+             {"Staff ID":3,"Name":"Rahul",
+              "Role":"peon","Salary":20000}
+             ]
+staff_timings ="morning 9AM to evening 5PM"
+principal_name ="Rakesh"
+Teacher_name ="Rai"
+poen_name ="Rahul"
+salary_of_Principal =50000
+salary_of_teacher =30000
+salary_of_peon =20000
+new_staff ={"Staff ID":4,"Name":"Laxmi",
+            "Role":"Clerk","Salary":15000}
+update_staff_salary =[{"Staff ID":1,"Name":"Ravi",
+              "Role":"Teacher","Salary":30000},
+             {"Staff ID":2,"Name":"Rakesh",
+              "Role":"Principal","Salary":50000},
+             {"Staff ID":3,"Name":"Rahul",
+              "Role":"peon","Salary":20000}
+             ]
+update_ravi_salary =+10000
+
+
+
+
+
+
+print(Staff_list)
+print(staff_timings)
+print("ALL Staff Details:")
+print(principal_name)
+print(Teacher_name)
+print(poen_name)
+print(salary_of_Principal)
+print(salary_of_teacher)
+print(salary_of_peon)
+print(new_staff)
+print(Staff_list)
+print(update_staff_salary)
+print(update_ravi_salary)
