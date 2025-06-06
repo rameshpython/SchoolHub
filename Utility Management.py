f55f3a8f17a798c884ca2a19ed6c1ca12d1bb8ab
rameshpython <rambabugaganam5515@gmail.com>
2025-06-06 11:50:23 +0530

    Add files via upload
    added utility management in python.py

---
+++ Utility Management.py
@@ -0,0 +1,28 @@
+#Utility management program in python with datatypes
+name ="Ramesh"
+house_number ="34/6"
+month ="June"
+#Utility bills in Rupees
+electricity_bill =1500
+water_bill =1200
+internet_bill =800
+gas_bill =900
+tv_bill =300
+others =500
+#calculate the total bill
+total_bill =electricity_bill+water_bill+internet_bill+gas_bill+tv_bill+others
+
+
+
+
+print("Utility Management Bill")
+print("Name:",name)
+print("House Number:",house_number)
+print("Month:",month)
+print("Electricity Bill:",electricity_bill)
+print("Water Bill:",water_bill)
+print("Internet Bill:",internet_bill)
+print("Gas Bill:",gas_bill)
+print("Tv Bill:",tv_bill)
+print("Others:",others)
+print("Total Bill:",total_bill)
