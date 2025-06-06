1e99ee4dce45ac47560e96d0ea08a217b1c263f9
rameshpython <rambabugaganam5515@gmail.com>
2025-06-06 13:00:25 +0530

    Add files via upload
    added school advertising management in python

---
+++ school advertise management.py
@@ -0,0 +1,25 @@
+#school advertising details
+school_name ="Sunrise High School"
+ad_medium ="News Paper"
+ad_cost =20000
+ad_date ="2025-06-05"
+ad_duration_days =7
+target_area ="Hyderabad"
+daily_cost = ad_cost/ad_duration_days
+ad_cost ={20000}
+ad_cost.add(50000)
+ad_cost.remove(20000)
+
+
+
+
+print("School Advertising Details")
+print("School Name:",school_name)
+print("Ad Medium:",ad_medium)
+print("Ad Cost:",ad_cost)
+print("Ad Date:",ad_date)
+print("Ad Duration Days:",ad_duration_days)
+print("Target Area:",target_area)
+print("Daily Cost:",daily_cost)
+print(ad_cost)
+print(ad_cost )
