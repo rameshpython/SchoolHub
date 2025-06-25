f1fb00274b42436532319bf95d7958f80760b328
rameshpython <ramababugaganam5515@gmail.com>
2025-06-25 10:29:41 +0530

    binarynumsystem

---
+++ examples/floatdatatypes.py
@@ -0,0 +1,50 @@
+#float Data types
+#Example 1:
+a=1.8
+b=7.3
+c=a+b
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print("-------------------------")
+#Example 2:
+a=10
+b=5.8
+c=a+b
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print("--------------------------")
+
+#Example 3:
+#'e' is used as shorthand for exponent of 10.
+a=3e2
+print(a,type(a))
+a=10e-2
+print(a,type(a))
+a=0.00000000000000000000000000000000000000000000000000000045
+print(a,type(a))
+a=2.1111111111111111111111111111111111111111111111111111111111111111111111111111111111112
+print(a,type(a))
+a=542.545526556436599564
+print(a,type(a))
+
+#'e' is used as shorthand for exponent of 10.
+#Examples :
+f=1.2e3
+print(f)
+f=214.54e9
+print(f)
+#3e2----3*10 exponent 2 = answer is 300
+# 10 exponent is that, The 10 to the power is 2
+a=3e2
+print(a)
+a=1.5e3
+print(a)
+a=2.1e-4
+print(a)
+
+
+
+
+
