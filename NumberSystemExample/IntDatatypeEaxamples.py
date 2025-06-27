c689a690dcde6596c7918821464fd232e23208eb
rameshpython <ramababugaganam5515@gmail.com>
2025-06-27 09:05:47 +0530

    added the html file for the number system example

---
+++ NumberSystemExample/IntDatatypeEaxamples.py
@@ -0,0 +1,48 @@
+#int Datatype examples :
+#Questions 1 to 7 . Level-1
+#1 - Convert decimal 25 to  binary,octal,and hexadecimal.
+  #Decimal to binary
+a=25
+bv=bin(a)
+print(bv,type(bv))
+
+  #Decimal to octal
+b=25
+ov=oct(b)
+print(ov,type(ov))#type of tell the data type
+
+  #Decimal to hexadecimal
+c=25
+hv=hex(c)
+print(hv,type(hv))
+
+#2-Convert decimal 64 to binary,octal,and hexadecimal.
+
+  #Decimal to binary
+a=64
+bv=bin(a)
+print(bv,type(bv))
+
+  #Decimal to octal
+b=64
+ov=oct(b)
+print(ov,type(ov))
+
+  #Decimal to hexadecimal
+c=64
+hv=hex(c)
+print(hv,type(hv))
+
+
+#3-Convert binary 0b1010 to decimal.
+a=0b1010
+decimal_number=int(a)
+print(decimal_number,type(decimal_number))
+
+#4-Convert octal 0o45 to decimal.
+a=0o45
+decimal_number=int(a)
+print(decimal_number,type(decimal_number))
+
+#5-Conver hexadecimal 0x2F
+
