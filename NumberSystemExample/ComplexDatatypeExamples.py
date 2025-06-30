a8d30c49a9bb337339252ef3569726d331b1fbcf
rameshpython <ramababugaganam5515@gmail.com>
2025-06-30 15:37:56 +0530

    Complex Datatypes

---
+++ NumberSystemExample/ComplexDatatypeExamples.py
@@ -0,0 +1,28 @@
+#Complex Datatype examples
+#Examples :
+#A Complex number as real and immaginary part.
+#1.What is a Complex datatype in python?Give an example.
+
+#A compex number is written as:
+
+#a + bj
+
+#a-->real part(float or int)
+#b-->imaginary(float  or int)
+#j-->imaginary unit(like root-1),used in python instead of i
+
+#creating a complex number
+num=3+4j
+print(num,type(num))
+#Accessing real and imaginary parts
+print("Real part:",num.real)
+print("imaginary part:",num.imag)
+
+#Another Example
+c=complex(6,9)
+print(c)
+
+#2.Write a python program to declare a  coplex number and display  its real and imaginary.
+
+
+
---
+++ examples/StrDatatypes.py
@@ -0,0 +1,60 @@
+#str datatypes
+#Examples :
+s1="Python"
+print(s1,type(s1))
+
+s2='Python'
+print(s2,type(s2))
+
+s3="A"
+print(s3,type(s3))
+
+s4='A'
+print(s4,type(s4))
+
+s1="123456"
+print(s1,type(s1))
+
+s2="Python3.134"
+print(s2,type(s2))
+
+s3="1234@#$%^&*"
+print(s3,type(s3))
+
+s4="!@#$%^&*987456321"
+print(s4,type(s4))
+
+s1='Python programing'
+print(s1,type(s1))
+
+s2="Gaganam ramesh"
+print(s2,type(s2))
+
+print("----------------------------------")
+
+#address1="Guido van Rossum #SyntaxError: unterminated string literal (detected at line 1)
+                            #here used double Quotes for string but finish point we not mentioned so got syntax error.
+
+#address1='Guido Van Rossun #SyntaxError: unterminated string literal (detected at line 1)
+                            #here used single Quotes for string but finish point we not mentioned so got syntax error.
+print("-----------------------------------")
+
+address1=" " "Guido Van Rossum FNO:5-8,Hill side Python software Foundation Nether Lands-69 " " "
+print(address1,type(address1))#here we used double Quotes
+
+address2=' ' 'Travis Oliphat HNO:5-8,Hill side Python software Foundation Nether Lands-58' ' '
+print(address2,type(address2))#here we used single Quotes
+
+s1=" " "Python Programming"
+print(s1,type(s1))
+
+s1=' ' 'Python Programming' ' '
+print(s1,type(s1))
+
+s2=" " "A" " "
+print(s2,type(s2))
+
+s2=' ' ' A' ' '
+print(s2,type(s2))
+
+
