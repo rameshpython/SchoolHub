da2664f1d6a8fed4445dba356699456147d3fd59
rameshpython <ramababugaganam5515@gmail.com>
2025-07-12 09:25:36 +0530

    inner or nested lists are added in list datatypes

--- examples/ListDatatypes.py
+++ examples/ListDatatypes.py
@@ -1,41 +1,28 @@
 #List Datatype :
->>> #list Datatypes
->>> list=[10,10.5,'durga',True,10]
->>> print(list)
+#list Datatypes
+list=[10,10.5,'durga',True,10]
+print(list)
 [10, 10.5, 'durga', True, 10]
->>> list=[10,20,30,40]
->>> list[0]
-10
->>> list[-1]
-40
->>> list[1:3]
-[20, 30]
->>> list[0]=100
->>> for i in list:
-...     print(i)
-...
-100
-20
-30
-40
->>>
->>> list=[10,20,30]
->>> list.append("durga")
->>> print(list)
+list=[10,20,30,40]
+list[0]
+list[-1]
+list[1:3]
+list[0]=100
+for i in list:
+  print(i)
+list=[10,20,30]
+list.append("durga")
+print(list)
 [10, 20, 30, 'durga']
->>>
->>> list.remove(20)
->>> print(list)
-[10, 30, 'durga']
->>>
->>> list2=list*2
->>> print(list2)
+list.remove(20)
+print(list)
+list2=list*2
+print(list2)
 [10, 30, 'durga', 10, 30, 'durga']
->>> list2=list*3
->>> print(list2)
-[10, 30, 'durga', 10, 30, 'durga', 10, 30, 'durga']
->>> #NOTE:An ordered,mutable,heterogenous collection of elements is nothing but list,where dublicates also allowed.
->>> #list is growable in nature based on our requirement we can increase or decrease the size.
+list2=list*3
+print(list2)
+#NOTE:An ordered,mutable,heterogenous collection of elements is nothing but list,where dublicates also allowed.
+#list is growable in nature based on our requirement we can increase or decrease the size.
 print("-------------------------------------------------")
 lst1=[10,20,30,40,10,20,40,10,60,70,1.2]
 print(lst1,type(lst1))
@@ -50,45 +37,79 @@ lst2[0:4]
 lst2[0]=1000
 print(lst2,type(lst2),id(lst2))
 print('--------------------------')
->>> lst=[10,"Rs",34.56,True]
->>> print(lst,type(lst))
-[10, 'Rs', 34.56, True] <class 'list'>
->>> lst[::2]
-[10, 34.56]
->>> lst[::2]=[100,45.67]
->>> print(lst)
-[100, 'Rs', 45.67, True]
->>>
->>> #Empty list[]
->>> lst1=[]
->>> print(lst1,type(lst1))
-[] <class 'list'>
->>> len(lst1)
-0
->>> lst1=lst()
-Traceback (most recent call last):
-  File "<python-input-34>", line 1, in <module>
-    lst1=lst()
-TypeError: 'list' object is not callable
->>>
+lst=[10,"Rs",34.56,True]
+print(lst,type(lst))
+lst[::2]
+lst[::2]=[100,45.67]
+print(lst)
+#Empty list[]
+lst1=[]
+print(lst1,type(lst1))
+len(lst1)
+#lst1=lst()
+#TypeError: 'list' object is not callable
 print("----------------------------")
 s="PYTHON"
 print(s,type(s))
-lst1=list(s)
+#lst1=list(s)
 print(lst1,type(lst1))
 len(lst1)
 
 s="PYTHON"
-lst=list([s])
+#lst=list([s])
 print(lst,type(lst))
 
 a=10
 print(a,type(a))
-lst1=list([a])
+#lst1=list([a])
 print(lst1,type(lst1))
-
-lst1=list(a)
 print(lst1)#TypeError: 'int' object is not iterable(means no more than one value)
+print("----------------------------------------------------------------------------------")
+#Inner list OR Nested list
+print("--------------------------------------------")
+lst=[10,'Rossum', [30,35,25] ,  [70,65,60]  , 'JNTU'  ]
+print(lst,type(lst))
+for val in lst:
+  print(val,"-->",type(val),"-->",type(lst))
+print(lst,type(lst))
+lst[2].append(38)
+print(lst,type(lst))
+lst[-2].insert(1,58)
+print(lst,type(lst))
+lst[-2][::3]
+# i want merge inner list values and append to existing list
+lst[2]+lst[3]
+[30, 35, 25, 38, 70, 58, 65, 60]
+print(lst)
+lst.insert(-1,lst[2]+lst[3])
+print(lst,type(lst))
+print(lst)
+lst[-2][1]=lst[-2][1]+lst[-2][1]*(50/100)
+print(lst)
+lst[-2][-2:]
+lst[-2][-2:]=[lst[-2][2]+lst[-2][2]*50/100, lst[-2][3]+lst[-2][3]*60/100]
+print(lst)
+del lst[-2]
+print(lst)
+lst[-2].pop()
+
+print(lst)
+del lst[2:4]
+print(lst)
+lst.insert(-1,[10,30,50])
+print(lst)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
