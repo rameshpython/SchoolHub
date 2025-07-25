3382b7522b3284228f03e2fc1b3469c2bca054fd
rameshpython <ramababugaganam5515@gmail.com>
2025-07-25 11:44:57 +0530

    Logical operator were added

---
+++ operators/LogicalOperators.py
@@ -0,0 +1,126 @@
+#Logical Operators :
+#and ,or ,not
+#and :
+True and False
+False and True
+False and False
+True and True
+
+#or
+True or False
+False or True
+False or False
+True or True
+
+#not
+not True
+not False
+
+#x and y
+
+10 and 20
+
+0 and 20
+#Rule of and.returns the second vvalue if both are True
+#return the first falsey value if any one is false
+
+#x or y
+
+10 or 20
+
+0 or 20
+#Rule of or.Returns the first truthy Value (both are True)
+#if both are falsy,returns tha last one.
+
+#not x
+not 10
+not 0
+
+#Example :
+"ramesh" and "gaganamramesh"
+"gaganamramesh" and "ramesh"
+" " and "ramesh"
+"ramesh" and " "
+" " or "ramesh"
+"ramesh" or " "
+not" "
+not "ramesh"
+
+
+print("--------------------------------------------------------------------------------------------------------------------------------")
+>>> #Logical Operators :
+>>> #and ,or ,not
+>>> #and :
+>>> True and False
+False
+>>> False and True
+False
+>>> False and False
+False
+>>> True and True
+True
+>>>
+>>> #or
+>>> True or False
+True
+>>> False or True
+True
+>>> False or False
+False
+>>> True or True
+True
+>>>
+>>> #not
+>>> not True
+False
+>>> not False
+True
+>>>
+>>> #x and y
+>>>
+>>> 10 and 20
+20
+>>>
+>>> 0 and 20
+0
+>>> #Rule of and.returns the second vvalue if both are True
+>>> #return the first falsey value if any one is false
+>>>
+>>> #x or y
+>>>
+>>> 10 or 20
+10
+>>>
+>>> 0 or 20
+20
+>>> #Rule of or.Returns the first truthy Value (both are True)
+>>> #if both are falsy,returns tha last one.
+>>>
+>>> #not x
+>>> not 10
+False
+>>> not 0
+True
+>>>
+>>> #Example :
+>>> "ramesh" and "gaganamramesh"
+'gaganamramesh'
+>>> "gaganamramesh" and "ramesh"
+'ramesh'
+>>> " " and "ramesh"
+'ramesh'
+>>> "ramesh" and " "
+' '
+>>> " " or "ramesh"
+' '
+>>> "ramesh" or " "
+'ramesh'
+>>> not" "
+False
+>>> not "ramesh"
+False
+
+
+
+
+
