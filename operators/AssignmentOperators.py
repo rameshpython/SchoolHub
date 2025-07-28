0f470997bc1ab97ddc4c8a947f30653087b4c6ba
rameshpython <ramababugaganam5515@gmail.com>
2025-07-28 11:54:47 +0530

    Added some more operators in Assigment operator today

--- operators/AssignmentOperators.py
+++ operators/AssignmentOperators.py
@@ -68,6 +68,124 @@ x **= 2  # same as x = x ** 2 → 3^2
 print(x)
 
 
+#8. &= → Bitwise AND and Assign
+
+#Bitwise AND(&) and assign operator
+a=1 & 0
+b=0 & 1
+c=0 & 0
+d=1 & 1
+print(a,b,c,d)
+
+
+a=16
+b=bin(a)
+print(b)
+
+a = 13        
+b = 6     
+c = a & b     
+print(c)
+
+a = 7   # binary: 0111
+b = 3   # binary: 0011
+
+a &= b  # same as a = a & b
+
+print(a)
+
+flags=15
+mask=13
+flags&=mask #-->flags=flags & mask
+print(flags)
+
+s={1,2,3}
+s&={2,3,4}
+print(s)
+
+s={1,2,3}
+s1={2,3,4}
+s&=s1
+print(s)
+
+
+
+
+
+#9. |= → Bitwise OR and Assign
+
+#Bitwise OR (|) and assign operator
+a= 1 | 0
+b= 0 | 1
+c= 0 | 0
+d= 1 | 1
+print(a,b,c,d)
+
+
+x = 12   # binary: 1100
+y = 5    # binary: 0101
+
+x &= y   # x = x & y
+
+print(x)
+print("------------------------")
+x = 1100  #(12 in binary)
+y =  101 #(5 in binary)
+  
+x &= y  #(4 in binary)
+print(x)
+
+
+a = 65     
+b = 58       
+a|= b   
+print(a)
+
+True | False     # → True (bitwise)
+True or False    # → True (logical)
+
+s1 = {1, 2}
+s2 = {2, 3}
+s1 |= s2
+
+#10. ^= → Bitwise XOR and Assign
+#The ^= is a compound assignment operator
+#a^=b --> a=a^b
+#Rule :Returns 1 only if the bits are different
+
+a=1 ^ 0 # a^=b
+
+b=0 ^ 1 #b^= 
+
+c=1 ^ 1 
+
+d=0 ^ 0
+
+print(a,b,c,d)
+
+
+a = 10 # 1010
+b = 7  # 0111
+
+a ^= b
+print(a)
+
+x = 12# 1100
+y = 5# 0101
+
+x ^= y# x = x ^ y
+print(x)
+
+#Swapping variables
+a = 5
+b = 7
+
+a ^= b #a=a^b
+b ^= a #b=b^a
+a ^= b #a=a^b
+
+print(a, b)
+
 
 
 
