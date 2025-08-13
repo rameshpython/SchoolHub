6d3f500ad2f04f6d42226509f92b70b40466ff7a
rameshpython <ramababugaganam5515@gmail.com>
2025-08-13 11:47:33 +0530

    For loops examples are added

--- StatementsAndLoops/Forloops.py
+++ StatementsAndLoops/Forloops.py
@@ -72,4 +72,174 @@ else:
     print("Loop finished!")
 
 
+##For loops more examples :
+s="python"
+print("by using for loop programing")
+i=0
+while(i<len(s)):
+    print("\t{}".format(s[i]))
+    i=i+1
+    print("by using for loop")
+    for ch  in s:
+        print("\t{}".format(ch))
+    else:
+        print("empty programing")
+        
+print("-"*50)
+
+#for loop
+s="python"
+print("By using for loop logic-1 forward direction")
+for ch in s:
+    print("\t{}".format(ch))
+
+print("-"*50)
+        
+print("By using for loop logic-2 forward direction with +ve index")
+for i in range (len(s)):
+    print("\t{}".format(s[i]))
+    
+print("-"*50)
+
+        
+print("By using for loop logic-3 forward direction with -ve index")
+for i in range(-len(s),0):
+    print("\t{}".format(s[i]))
+
+print("-"*50)
+        
+print("by using for loop logic-4 backward direction ")
+for ch in s[::-1]:
+    print("\t{}".format(ch))
+    
+print("-"*50)
+
+print("By using for loop logic-5 backward ddirection with +ve index")
+for i in range(len(s)-1,-1,-1):
+    print("\t{}".format(s[i]))
+
+print("-"*50)
+
+print("By using for loop logic-6 backward direction with -ve index")
+for i in range(-1,-(len(s)+1),-1):
+    print("\t{}".format(s[i]))
+    
+print("-"*50)
+
+##program for Displaying the Values of List and dict by using for loop
+lst=[100,"swathi","Delhi",34.56,"python"]
+for value in lst:
+    print("\t{}".format(value))
+
+print("-"*50)
+                                                  
+d={10:"mango",20:"Apple",30:"sberry",40:"kiwi"}
+for x in d:
+    print("\t{}--->{}".format(x,d[x]))
+
+print("-"*50)
+
+d={10:"mango",20:"Apple",30:"sberry",40:"kiwi"}   
+for x in d:
+    print("\t{}--->{}".format(x,d.get(x)))
+
+
+#for loop
+n=int(input("Enter How Many Numbers u want to generate:"))
+if(n<=0):
+    print("\t{} is Invalid Input".format(n))
+else:
+    print("List of  Numbers within:{}".format(n))
+    for i in range(1,n+1):
+        print("\t{}".format(i))
+print("------------------------------------------")
+print("List of Even Numbers within:{}".format(n))
+for i in range(2,n+1,2):
+    print("\t{}".format(i))
+print("------------------------------------------")
+print("List of Odd Numbers within:{}".format(n))
+for i in range(1,n+1,2):
+    print("\t{}".format(i))
+print("------------------------------------------")
+
+#To generate tables with for loops
+n=int(input("Enter a Number for Generating Mul table:"))
+if(n<=0):
+    print("\t{} is Invalid Input".format(n))
+else:
+    print("-"*50)
+    print("Mul Table for {}".format(n))
+    print("-" * 50)
+    for i in range(1,11):
+        print("\t{} x {}={}".format(n,i,n*i))
+    else:
+        print("-" * 50)
+
+#Program for Finding sum of First N Natural Numbers.
+n=int(input("Enter How Natural Nums Sum U want to Find:"))
+if(n<=0):
+    print("\t{} is Invalid Input".format(n))
+else:
+    print("-"*50)
+    print("Natural Numbers within:{}".format(n))
+    p=0 # Additive Identity--Keeps Track of repeated sum
+    print("-" * 50)
+    for i in range(1,n+1):
+        print("\t{}".format(i))
+        p=p+i
+    else:
+        print("-"*50)
+        print("sum={}".format(p))
+        print("-" * 50)
+
+
+#Program for Finding sum of First N Natural Numbers
+# also find Sum of Square and Sum of Cubes First N Natural Numbers.
+n=int(input("Enter How Natural Nums U want to Find:"))
+if(n<=0):
+    print("\t{} is Invalid Input".format(n))
+else:
+    print("-"*50)
+    print("Nat Nums\t\tSquares\t\t\tCubes")
+    s,ss,cs=0,0,0 # Additive Identity--Keeps Track of repeated sum
+    print("-" * 50)
+    for i in range(1,n+1):
+        print("\t{}\t\t\t\t{}\t\t\t{}".format(i,i**2,i**3))
+        s=s+i
+        ss=ss+i**2
+        cs=cs+i**3
+    else:
+        print("-"*50)
+        print("\t{}\t\t\t\t{}\t\t\t{}".format(s,ss,cs))
+        print("-" * 50)
+
+#Program for Finding Product of First N Natural Numbers.
+n=int(input("Enter How Natural Nums Product U want to Find:"))
+if(n<=0):
+    print("\t{} is Invalid Input".format(n))
+else:
+    print("-"*50)
+    print("Natural Numbers within:{}".format(n))
+    p=1 
+    print("-" * 50)
+    for i in range(1,n+1):
+        print("\t{}".format(i))
+        p=p*i
+    else:
+        print("-"*50)
+        print("Product={}".format(p))
+        print("-" * 50)
+
+#Program for Finding Fcatorial of a Number
+n=int(input("Enter a Number for Finding Factorial:"))
+if(n<0):
+    print("\t{} is -VE Value--can't cal Factrial".format(n))
+else:
+    fact=1
+    for i in range(1,n+1):
+        fact*=i # OR fact=fact*i
+    else:
+        print("factroail({})={}".format(n,fact))
+
+
 
