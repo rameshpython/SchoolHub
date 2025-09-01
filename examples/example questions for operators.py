fc92d8fde37362b41e76dab230a1f444580c321c
rameshpython <ramababugaganam5515@gmail.com>
2025-09-01 19:15:26 +0530

    operators private question answer are added

---
+++ examples/example questions for operators.py
@@ -0,0 +1,171 @@
+#Q1. What is the result of 10 + 3 in Python?
+a=10
+b=3
+print(a+b)
+
+a=int(input("Enter first value :"))
+b=int(input("Enter second value:"))
+print("\t{}+{}={}".format(a,b,a+b))
+print(a+b)
+print("-----------------------------------------")
+#Q2. What is the result of 10 - 3 in Python?
+a=10
+b=3
+print(a-b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}-{}={}".format(a,b,a-b))
+print("-----------------------------------------")
+#Q3. What is the result of 10 * 3 in Python?
+a=10
+b=3
+print(a*b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}*{}={}".format(a,b,a*b))
+
+print("-----------------------------------------")
+
+#Q4. What is the result of 10 / 3 in Python?
+a=10
+b=3
+print(a/b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}/{}={}".format(a,b,a/b))
+print("-----------------------------------------")
+#Q5. What is the result of 10 // 3 in Python?
+a=10
+b=3
+print(a//b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}//{}={}".format(a,b,a//b))
+print("-----------------------------------------")
+#Q6. What is the result of 10 % 3 in Python?
+a=10
+b=3
+print(a%b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}%{}={}".format(a,b,a%b))
+print("-----------------------------------------")
+#Q7. What is the result of 10 ** 3 in Python?
+a=10
+b=3
+print(a**b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}**{}={}".format(a,b,a**b))
+
+#Q8. What is the result of 20 + 5 in Python?
+a=20
+b=5
+print(a+b)
+
+a=int(input("Enter first value :"))
+b=int(input("Enter second value:"))
+print("\t{}+{}={}".format(a,b,a+b))
+print(a+b)
+print("-----------------------------------------")
+#Q9. What is the result of 20 - 5 in Python?
+a=20
+b=5
+print(a-b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}-{}={}".format(a,b,a-b))
+print("-----------------------------------------")
+#10. What is the result of 20 * 5 in Python?
+a=20
+b=5
+print(a*b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}*{}={}".format(a,b,a*b))
+
+print("-----------------------------------------")
+
+#11. What is the result of 20/ 5 in Python?
+a=20
+b=5
+print(a/b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}/{}={}".format(a,b,a/b))
+print("-----------------------------------------")
+#12. What is the result of 20 // 5 in Python?
+a=20
+b=5
+print(a//b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}//{}={}".format(a,b,a//b))
+print("-----------------------------------------")
+#13. What is the result of 20 % 5 in Python?
+a=20
+b=5
+print(a%b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}%{}={}".format(a,b,a%b))
+print("-----------------------------------------")
+#14. What is the result of 20 ** 5 in Python?
+a=20
+b=5
+print(a**b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}**{}={}".format(a,b,a**b))
+
+#15Q.What is the result of 20 + 5 in Python?
+a=15
+b=5
+print(a+b)
+
+a=int(input("Enter first value :"))
+b=int(input("Enter second value:"))
+print("\t{}+{}={}".format(a,b,a+b))
+print(a+b)
+print("-----------------------------------------")
+#16Q. What is the result of 10 - 3 in Python?
+a=45
+b=25
+print(a-b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}-{}={}".format(a,b,a-b))
+print("-----------------------------------------")
+#17Q. What is the result of 10 * 3 in Python?
+a=15
+b=2
+print(a*b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}*{}={}".format(a,b,a*b))
+
+print("-----------------------------------------")
+
+#18Q. What is the result of 10 / 3 in Python?
+a=10
+b=2
+print(a/b)
+
+a=int(input("Enter value of a:"))
+b=int(input("Enter  value of b:"))
+print("{}/{}={}".format(a,b,a/b))
+
